f9a5db81bfcb0ea01d5b1449b415e7c3b3b43fd9
CHRISTOROMO <99474042+CHRISTOROMO@users.noreply.github.com>
2022-05-12 12:05:31 +0300

    Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import random
 from turtle import color
-import streamlit as st
 import _tkinter
+import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
